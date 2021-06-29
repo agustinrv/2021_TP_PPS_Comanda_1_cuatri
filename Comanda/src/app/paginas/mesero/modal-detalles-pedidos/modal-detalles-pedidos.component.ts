cb5531edc95d9e81c2d5c02dccf87322fbe4dc60
agustinrv <Agustinrivola2000@gmail.com>
2021-06-29 14:01:51 -0300

    agrego funcion para que el mozo elimine el pedido en cualquier momento

--- Comanda/src/app/paginas/mesero/modal-detalles-pedidos/modal-detalles-pedidos.component.ts
+++ Comanda/src/app/paginas/mesero/modal-detalles-pedidos/modal-detalles-pedidos.component.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import { Pedido } from 'src/app/clases/pedido/pedido';
 import { EestadoPedido } from 'src/app/enumerados/EestadoPedido/eestado-pedido';
 import { PedidosService } from 'src/app/servicios/pedidos/pedidos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modal-detalles-pedidos',
@@ -48,12 +49,39 @@ export class ModalDetallesPedidosComponent implements OnInit {
 
     return tienePedidosSinTerminar;
   }
+  public CancelarPedido(){
+    this.Confirmar('Si lo cancela no podra recuperarlo',"Desea continuar?",'Si,Eliminalo').then((result) => {
+      if (result.isConfirmed) {
+        this.servicioPedidos.BorrarUno(this.pedidoSeleccionado);  
+        Swal.fire(
+          'Eliminado',
+          'El cliente debe volver a cargar el pedido',
+          'success'
+          );
+          this.CerrarModal();
+      }
+    });
+  }
 
 
   public EntregarPedido(){
       this.pedidoSeleccionado.estadoPedido=EestadoPedido.Entregado;      
       this.servicioPedidos.ModificarUno(this.pedidoSeleccionado);
       this.CerrarModal();
+  }
+
+  public Confirmar(titulo:string,texto:string,botonConfirmar:string){
+    return Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3ca841',
+      cancelButtonColor: '#d33',
+      confirmButtonText: botonConfirmar,
+      cancelButtonText: 'Cancelar'
+    });
+
   }
 
 }
