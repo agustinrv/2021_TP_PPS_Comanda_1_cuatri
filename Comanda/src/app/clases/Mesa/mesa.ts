48d34d34f93bd562ca6c23450930f4fd45229a3f
agustinrv <Agustinrivola2000@gmail.com>
2021-07-02 16:41:19 -0300

    limpiar mesas metre

--- Comanda/src/app/clases/Mesa/mesa.ts
+++ Comanda/src/app/clases/Mesa/mesa.ts
@@ -3,7 +3,7 @@ import { Cliente } from "../Cliente/cliente";
 
 export class Mesa {
     public id:string;
-    public cliente:Cliente;//o correo
+    public cliente:Cliente;
     public numero:number;//es el numero de la mesa,en principio solo se manipula en BD hasta que se creemos el alta
     public cantidadDeComensales:number;
     public tipo:EtipoMesa;
